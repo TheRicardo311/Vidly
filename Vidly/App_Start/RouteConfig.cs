128e4a5673b983970d3c94234d12a041f18029da
Ricardo van Oldenbarneveld genaamd Witte Tullingh <r.vanoldenbarneveld@live.nl>
2017-11-08 17:08:14 +0100

    start

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -9,6 +9,7 @@ namespace Vidly
 {
     public class RouteConfig
     {
+        //start
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
